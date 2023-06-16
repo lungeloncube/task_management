d8ccd3aef5679f59967ac30eae7c4fa459dea2c2
Tendai Nyasha Mupezeni <nmupezeni@gmail.com>
2023-06-16 18:16:34 +0200

    Task Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Task task={{ title: "Task 1", description: "Description 1", id: 1 }} />
       </header>
     </div>
   );
